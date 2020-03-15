3b5a7d24ae13f256c7a9fc262d361a07aaa65aa4
Sven.Wong <Sven.Wong@homecredit.cn>
2020-03-15 23:51:20 +0800

    remove so adjust mk

--- AndroidMk/app/src/main/jni/Android.mk
+++ AndroidMk/app/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 #LOCAL_MODULE的名称要和之前System.loadLibrary("hello-jni");中的名称一致;
 LOCAL_MODULE := hello-jni
-LOCAL_SRC_FILES := hello-jni.c
+LOCAL_SRC_FILES := hello-jni.c add.c mul.c sub.c
 
 #LOCAL_LDLIBS就是天机库用的
 # -l 是默认的 log就是log库
